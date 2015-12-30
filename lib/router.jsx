eaac2c086e30ecfbf2deaff426eedb49dc73c0f1
Korus <tdwells90@Toms-MBP.home>
2015-12-30 12:43:12 +0000

    Change to React-router

--- lib/router.jsx
+++ lib/router.jsx
@@ -1,13 +1,1 @@
-FlowRouter.route('/', {
-  action() {
-    ReactLayout.render(App);
-  },
-  fastRender: true
-});
 
-FlowRouter.route('/login', {
-  action(params) {
-    ReactLayout.render(Login);
-  },
-  fastRender: true
-});
