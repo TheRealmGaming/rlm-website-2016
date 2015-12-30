4b43974941baedfa781fac9f6c0c7b095f5f9093
Korus <tdwells90@Toms-MBP.home>
2015-12-30 22:45:59 +0000

    Re-organise structure

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -7,24 +7,22 @@ Login = React.createClass({
   },
   render() {
     return (
-      <div className="row">
-        <div className="login-area">
-          <h2 className="page-header">Login</h2>
-          <form id="login" className="login" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="emailAddress">Email Address</label>
-              <input type="email" name="emailAddress" className="form-control" placeholder="Email Address" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password"><span className="pull-left">Password</span> <a className="pull-right" href="/recover-password"> Forgot Password?</a></label>
-              <input type="password" name="password" className="form-control" placeholder="Password" />
-            </div>
-            <div className="form-group">
-              <input type="submit" className="btn btn-success" value="Login" />
-            </div>
-          </form>
-          <p>Don't have an account? <a href="/signup">Sign Up</a>.</p>
-        </div>
+      <div className="login-area">
+        <h2 className="page-header">Login</h2>
+        <form id="login" className="login" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="emailAddress">Email Address</label>
+            <input type="email" name="emailAddress" className="form-control" placeholder="Email Address" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password"><span className="pull-left">Password</span> <a className="pull-right" href="/recover-password"> Forgot Password?</a></label>
+            <input type="password" name="password" className="form-control" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-success" value="Login" />
+          </div>
+        </form>
+        <p>Don't have an account? <a href="/signup">Sign Up</a>.</p>
       </div>
     );
   }
