28877eb7448d3e67523536394b77650d1d7df0b1
korus90 <feetw@ENV1-237.ds.leeds.ac.uk>
2016-01-05 16:51:31 +0000

    Image upload permissions

--- lib/collections/avatars.jsx
+++ lib/collections/avatars.jsx
@@ -1,9 +1,3 @@
 Avatars = new FS.Collection("avatars", {
   stores: [new FS.Store.FileSystem("avatars")]
 });
-
-Avatars.allow({
-  insert: function() { return true; },
-  update: function() { return true; },
-  download: function(userId, fileObj) { return true; }
-});
---
+++ server/permissions.jsx
@@ -0,0 +1,5 @@
+Avatars.allow({
+  insert: function() { return true; },
+  update: function() { return true; },
+  download: function(userId, fileObj) { return true; }
+});
